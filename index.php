bce9395dd54abc18d1a013511a40a78d337a055e
Hugo Mesquita <hugomesquitaweb@gmail.com>
2017-10-10 16:30:48 -0400

    primeiro commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<title>Background - Icone em Movimento</title>
+	<link rel="stylesheet" href="assets/css/index.css">
+</head>
+<body>
+	<header id="topo">
+		
+
+		<div class="intro-scroller">
+			<a href="" class="inner-link">
+				<div class="mouse-icon" style="opacity: 1;">
+					<div class="wheel"></div>
+				</div>
+			</a>
+		</div>
+	</header>
+</body>
+</html>
